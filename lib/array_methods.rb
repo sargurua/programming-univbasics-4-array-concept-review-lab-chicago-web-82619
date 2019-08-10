8acb37e1be8f02cd7dd9958c4c75def462e17b36
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-10 04:55:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  array.length.time{ |index|
+  array.length.times{ |index|
     if array[index] == value_to_find
       return index
     end
